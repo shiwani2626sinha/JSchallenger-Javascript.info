4ef5098cdde3a1f8a6f0ba74521c328104bb1f09
shiwani sinha <35289975+shiwani2626sinha@users.noreply.github.com>
2021-06-26 22:56:45 +0530

    Add files via upload

---
+++ OPS.js
@@ -0,0 +1,22 @@
+// sol 1- 
+
+var ans = 0 ;
+if(a<b)
+{
+   ans = a/b;
+}
+else {
+   ans = a*b;
+}
+return ans;
+
+// ninja sol 2-
+var ans = 0 ;
+if(a<b)
+{
+   ans = a/b;
+}
+else {
+   ans = a*b;
+}
+return ans;
