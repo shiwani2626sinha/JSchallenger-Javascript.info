b251a0739afb3259521fa926ad6cb302fdcc6bf3
shiwani sinha <35289975+shiwani2626sinha@users.noreply.github.com>
2021-07-07 00:55:55 +0530

    Create Javascript.info_q4.js

---
+++ Javascript.info_q4.js
@@ -0,0 +1,23 @@
+/*implementation of constructor and JS Engine nature */
+
+function Calculator () {
+  this.read= function () {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+ };
+ this.sum = function() {
+   return this.a + this.b;
+ }
+ ;
+ this.mul = function() {
+ return this.a * this.b;
+ };
+
+}
+
+//driver
+let calculator = new Calculator();
+ calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
