9e71538e207335383cd599b7610d92172eba06e8
shiwani sinha <35289975+shiwani2626sinha@users.noreply.github.com>
2021-07-10 23:16:49 +0530

    Create Javascript.info_custom_error.js

---
+++ Javascript.info_custom_error.js
@@ -0,0 +1,33 @@
+class ValidCheckError extends Error {
+  constructor (message){
+    super(); // calling parent - error class ctr is imp
+    this.name = "validation error"
+  }
+}
+// Usage
+function read(json) {
+  let user = JSON.parse(json);
+
+  if (!user.age) {
+    throw new ValidationError("No field: age");
+  }
+  if (!user.name) {
+    throw new ValidationError("No field: name");
+  }
+
+  return user;
+}
+
+// Working example with try..catch
+
+try {
+  let user = read('{ "age": 25 }');
+} catch (err) {
+  if (err instanceof ValidationError) {
+    alert("Invalid data: " + err.message); // Invalid data: No field: name
+  } else if (err instanceof SyntaxError) { // (*)
+    alert("JSON Syntax Error: " + err.message);
+  } else {
+    throw err; // unknown error, rethrow it (**)
+  }
+}
