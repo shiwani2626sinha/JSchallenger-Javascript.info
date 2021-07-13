ad64f8576389fb9adb3adb16787c202763a29bfa
shiwani sinha <35289975+shiwani2626sinha@users.noreply.github.com>
2021-07-14 04:54:55 +0530

    Update Javascript.info_PROMISE.js

--- Javascript.info_PROMISE.js
+++ Javascript.info_PROMISE.js
@@ -2,7 +2,7 @@
 // IT TAKES A FUNCTION AS A PARAMETER WHILE OBJECT CREATION
 // FUNCTION IS THE PRODUCER CODE
 // FUNCTION HAS TWO PARAMETERS - RESOLVE / REJECT
-// ONCE FUNCTION EXECUTION IS COMPLETED, IF THERE'S AN ERROR, IT CALLS REJECT CODE ELSE RESOLVE CODE
+// ONCE FUNCTION EXECUTION IS COMPLETED, IF THERE'S AN ERROR, IT CALLBACK THE REJECT CODE ELSE RESOLVE CODE
 
 
 // HOW TO CREATE A PROMISE - 
@@ -11,4 +11,26 @@ let promise = new Promise(function(resolve, reject) {
   // this is more like a performer
 
 }); 
+
+
+//promise object properties
+// a promise object has state property - which is initially pending, fulfilled when resolved else rejected
+// a promise object has result property - which is initially undefined, changed to result or error
+// executer will run the code and callback resolve / reject to change the state of the promise object
+let promise = new Promise(function(resolve, reject) {
+ //execution code
+ // success
+ resolve("success");
+});
+
+let promise = new Promise(function(resolve,reject){
+ //execution cde
+ //failes
+ reject("whhooops! ")
+ //
+});
+
+//Accessing or subscribing to a promise object
+// how can a consumer use a async function created through the promise object
+// then/catch/finally
 
