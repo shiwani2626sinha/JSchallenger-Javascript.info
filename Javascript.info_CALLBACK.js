2b7f156d05bf35c35fb33eb119662cac3a37f804
shiwani sinha <35289975+shiwani2626sinha@users.noreply.github.com>
2021-07-13 13:42:57 +0530

    Update Javascript.info_CALLBACK.js

--- Javascript.info_CALLBACK.js
+++ Javascript.info_CALLBACK.js
@@ -18,5 +18,47 @@ loadScript("some-url-scr", (srcipt) =>
         //code again... this mutiple times lead to a PYRAMID 
       })
 })
-           
+      
+
+// callback error handling 
+function loadScript(src, callback){
+  //code of script loading
+  script.onload = () => callback(null, srcipt);
+  script.onerror = () => callback(error, null );
+  //code of script appending
+}
+
+loadScript("some-random-url-here-we-go-again", function(error, srcipt){
+  if(error){
+    //some error handle code 
+  }
+  else{
+    //some actual work driven
+  }
+})
+
+
+// callback hell problem or pyramid of doom 
+// so if we keep calling call back inside on another and keep on handling cases and loops it will become a pyramid structure tightly coupled
+//something like this - 
+loadScript("some-random-url-here-we-go-again", function(error, srcipt){
+  if(error){
+    //some error handle code 
+  }
+  else{
+    //code
+    loadScript("some-random-url", function (error, script) {
+      if(error){
+        //handle the error
+      }
+      else {
+        //code
+        loadScript("some-ul", function(error,script){
+         ...................................
+        })
+      }
+    })
+  }
+})
+
            
