8120a878b836d1f846195f184600a4c88ad6b403
shiwani sinha <35289975+shiwani2626sinha@users.noreply.github.com>
2021-07-15 12:05:20 +0530

    Update Javascript.info_promise_Chaining2.js

--- Javascript.info_promise_Chaining2.js
+++ Javascript.info_promise_Chaining2.js
@@ -1,2 +1,15 @@
 // return promise in chaining
-let
+// notice that .then() returns a promise object
+let promise = new Promise(function(resolve, reject){
+  resolve(1);
+})
+
+promise.then( function(result) {
+  alert(result)  //1 
+  return new Promise(function(resolve, reject){
+    resolve(result*2);
+  });
+}
+).then (function(result){
+alert(result)  //2 
+})
